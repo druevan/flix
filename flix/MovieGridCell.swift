0797c10394f735401b8485f6cf5570183b20dafb
Dru Grossberg <druevan@gmail.com>
2019-10-27 21:54:05 -0400

    flix 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Dru Grossberg on 10/27/19.
+//  Copyright © 2019 Dru Grossberg. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
